4f847800cf74e8b1b80cfc91c11549f0b5180b14
FRED NYAKAGWA <fnyakagwa@mo-de.local>
2021-05-07 22:22:08 +0300

    add user model and connect to db

---
+++ config/db.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+	try {
+		const conn = await mongoose.connect('mongodb://localhost/userData', {
+			useNewUrlParser: true,
+			useFindAndModify: false,
+			useCreateIndex: true,
+		});
+
+		console.log(
+			`Mongo DB Connected on ${conn.connection.host}`.cyan.underline.bold,
+		);
+	} catch (err) {
+		console.error(`Error: ${err.message}`.red.underline.bold);
+		process.exit(1);
+	}
+};
+
+export default connectDB;
--- server.js
+++ server.js
@@ -1,30 +1,41 @@
-const express= require('express');
-const mongoose= require('mongoose');
-const bodyParser= require('body-parser');
-const port=8000;
-const app= express();
+import express from 'express';
+import bodyParser from 'body-parser';
+import chalk from 'chalk';
+import User from './models/User.js';
+import connectDB from './config/db.js';
+import colors from 'colors';
+
+const port = 8000;
+const app = express();
+
+connectDB();
 
 app.use(bodyParser.json());
 
-app.listen(port, ()=>{
-	console.log(`server is listening on port:${port}`)
-})
+app.listen(port, () => {
+	try {
+		console.log(chalk.yellow.inverse.bold(`server is on port:${port}`));
+	} catch (err) {
+		console.log(err);
+	}
+});
 
 // CREATE
-app.post('/users',(req,res)=>{
-  // User.create()
-})
+app.post('/users', (req, res) => {
+	// User.create()
+});
 
-app.route('/users/:id')
-// READ
-.get((req,res)=>{
-  // User.findById()
-})
-// UPDATE
-.put((req,res)=>{
-  // User.findByIdAndUpdate()
-})
-// DELETE
-.delete((req,res)=>{
-  // User.findByIdAndDelete()
-})
+app
+	.route('/users/:id')
+	// READ
+	.get((req, res) => {
+		// User.findById()
+	})
+	// UPDATE
+	.put((req, res) => {
+		// User.findByIdAndUpdate()
+	})
+	// DELETE
+	.delete((req, res) => {
+		// User.findByIdAndDelete()
+	});
